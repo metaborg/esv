39de779b64dcd970eeffdf15185b0f21f96160a4
Gabriel Konat <gabrielkonat@gmail.com>
2020-10-08 14:26:57 +0200

    Add Gradle build file for ESV.

---
+++ org.metaborg.meta.lang.esv/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  id("org.metaborg.spoofax.gradle.langspec")
+  id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
+  `maven-publish`
+}
+
+ecj {
+  toolVersion = "3.21.0"
+}
+tasks.withType<JavaCompile> { // ECJ does not support headerOutputDirectory (-h argument).
+  options.headerOutputDirectory.convention(provider { null })
+}
+
+// HACK: Set different group to prevent substitution of the baseline version to this project. I could not find another
+// way to disable this substitution.
+group = "org.metaborg.bootstraphack"
