90fed5e3058ba724fc0532e2c7d65f1315ff4a40
Gabriel Konat <gabrielkonat@gmail.com>
2021-06-04 11:36:32 +0000

    Merge branch 'master' into spoofax-release

---
+++ org.metaborg.meta.lang.esv/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+  id("org.metaborg.devenv.spoofax.gradle.langspec")
+  id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
+  `maven-publish`
+}
+
+// Replace source dependencies with overridden/local ones.
+fun compositeBuild(name: String) = "$group:$name:$version"
+val spoofax2BaselineVersion: String by ext
+val spoofax2Version: String by ext
+spoofaxLanguageSpecification {
+  addCompileDependenciesFromMetaborgYaml.set(false)
+  addSourceDependenciesFromMetaborgYaml.set(false)
+}
+dependencies {
+  compileLanguage("org.metaborg:org.metaborg.meta.lang.esv:$spoofax2BaselineVersion")
+  compileLanguage(compositeBuild("org.metaborg.meta.lang.template"))
+
+  sourceLanguage(compositeBuild("meta.lib.spoofax"))
+}
+
+ecj {
+  toolVersion = "3.21.0"
+}
+tasks.withType<JavaCompile> { // ECJ does not support headerOutputDirectory (-h argument).
+  options.headerOutputDirectory.convention(provider { null })
+}
